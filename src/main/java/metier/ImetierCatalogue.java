851e9e677a16a4ad903abd6f18a511f311ca9d67
yzbaya <101043195+yzbaya@users.noreply.github.com>
2023-02-28 22:25:59 +0100

    Initial commit

---
+++ src/main/java/metier/ImetierCatalogue.java
@@ -0,0 +1,7 @@
+  package metier;
+    import java.util.List;
+    
+        public interface ImetierCatalogue {
+        public List<Produit> getProduitsParMotCle(String mc);
+        public void addProduit(Produit p);
+       }
